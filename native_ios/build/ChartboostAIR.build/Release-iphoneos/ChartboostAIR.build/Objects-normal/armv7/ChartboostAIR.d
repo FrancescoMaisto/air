a30f65e522faf700abcb93f911e8885070ed9357
Brian Hansen <brian.hansen@chartboost.com>
2014-03-10 10:59:48 -0700

    working on ios sdk 4.1 update

---
+++ native_ios/build/ChartboostAIR.build/Release-iphoneos/ChartboostAIR.build/Objects-normal/armv7/ChartboostAIR.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/brianhansen/Documents/SDK/air/native_ios/ChartboostAIR/ChartboostAIR.m \
+  /Users/brianhansen/Documents/SDK/air/native_ios/ChartboostAIR/ChartboostAIR.h \
+  /Users/brianhansen/Documents/SDK/air/native_ios/AIR/FlashRuntimeExtensions.h \
+  /Users/brianhansen/Documents/SDK/air/native_ios/ChartboostAIR/ChartboostWrapper.h \
+  /Users/brianhansen/Documents/SDK/air/native_ios/Chartboost/Chartboost.h
---
+++ native_ios/build/ChartboostAIR.build/Release-iphoneos/ChartboostAIR.build/Objects-normal/armv7/ChartboostWrapper.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/brianhansen/Documents/SDK/air/native_ios/ChartboostAIR/ChartboostWrapper.m \
+  /Users/brianhansen/Documents/SDK/air/native_ios/ChartboostAIR/ChartboostWrapper.h \
+  /Users/brianhansen/Documents/SDK/air/native_ios/Chartboost/Chartboost.h \
+  /Users/brianhansen/Documents/SDK/air/native_ios/AIR/FlashRuntimeExtensions.h
